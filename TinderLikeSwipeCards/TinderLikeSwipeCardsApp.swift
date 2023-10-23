446809bff339883aa3676c1a6e440322e13fcf68
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-10-23 10:22:54 +0200

    Initial Commit

---
+++ TinderLikeSwipeCards/TinderLikeSwipeCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderLikeSwipeCardsApp.swift
+//  TinderLikeSwipeCards
+//
+//  Created by Kaori Persson on 2023-10-23.
+//
+
+import SwiftUI
+
+@main
+struct TinderLikeSwipeCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
